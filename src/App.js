b4149cf8d35cadcdbcb1528d63e71dfd75d4b45b
K Kasi <you@example.com>
2020-01-01 21:15:12 +0530

    update Data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import ExJsplumb from './ExJsplumb14/Example14'; // Change Example name
+import ExJsplumb from './ExJsplumb6'; // Change Example name
 
 class App extends React.Component{
   render() {
